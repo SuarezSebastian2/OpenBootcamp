de8d302375f3c376b129aed9a74f7bf2d2a9e1ef
S-Andres-Suarez <sebastian.suarez@ofi.com.co>
2023-01-03 12:36:43 -0500

    Ejercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,55 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Coche cocheP = new Coche();
 
-        int resultant;
-        resultant = suma(20, 30, 2);
+        //Punto 1
+       int numeroIf = 0;
 
-        System.out.println("El resultante de la suma es: "+resultant);
+       if (numeroIf > 0){
+           System.out.println("NumeroIf es Positivo");
+       }if (numeroIf < 0){
+            System.out.println("NumeroIf es Negativo");
+        }if (numeroIf == 0){
+            System.out.println("NumeroIf es 0");
+        }
 
-        cocheP.añadirPuerta();
-        System.out.println("El numero de puertas del coche es de: "+cocheP.numeroPuertas);
-        cocheP.añadirPuerta();
-        System.out.println("El numero de puertas del coche es de: "+cocheP.numeroPuertas);
-        cocheP.añadirPuerta();
-        System.out.println("El numero de puertas del coche es de: "+cocheP.numeroPuertas);
+       //Punto 2
+       int numeroWhile = 1;
+       while (numeroWhile < 3){
+           System.out.println("NumeroWhile es: "+numeroWhile);
+           numeroWhile = numeroWhile + 1;
+       }
 
-    }
+       //Punto 3
+       int numeroDoWhile = 2;
+       do {
+           System.out.println("NumeroDoWhile es: "+numeroDoWhile);
+           numeroDoWhile = numeroDoWhile + 3;
+       }while (numeroWhile < 3);
 
+       //Punto 4
+       for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
+           System.out.println("NumeroFor es: "+numeroFor);
+        }
 
-    public static int suma(int a, int b, int c) {
-        return  a + b + c;
+       //Punto 5
+       String Estacion = "Verano";
+       //Verano, Invierno, Otoño, Primavera
+       switch (Estacion){
+           case "Verano":
+               System.out.println("Verano");
+               break;
+           case "Invierno":
+               System.out.println("Invierno");
+               break;
+           case "Otoño":
+               System.out.println("Otoño");
+               break;
+           case "Primavera":
+               System.out.println("Primavera");
+               break;
+           default:
+               System.out.println(Estacion);
+       }
     }
 }
