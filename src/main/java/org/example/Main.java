ead96c7a523f4dd7c0b62e24bac00b08bdd4f23b
S-Andres-Suarez <sebastian.suarez@ofi.com.co>
2023-01-04 12:02:23 -0500

    Ejercicio 4.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         Persona persona1 = new Persona();
+        Cliente cliente1 = new Cliente();
+        Trabajador trabajador1 = new Trabajador();
+
         persona1.setEdad(20);
         persona1.setNombre("Carlos");
         persona1.setTelefono(3000);
+        cliente1.setCredito(500);
+        trabajador1.setSalario(2000);
         System.out.println("La edad es: "+persona1.getEdad()+"\n"+"El numero de telefono es: "+
-                persona1.getTelefono()+"\n"+"El nombre es: "+persona1.getNombre());
+                persona1.getTelefono()+"\n"+"El nombre es: "+persona1.getNombre()+"\n"+"El credito es de: "+
+                cliente1.getCredito()+"\n"+"El salario es "+trabajador1.getSalario());
     }
 }
