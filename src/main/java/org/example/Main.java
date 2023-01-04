bc965de2d10e234206c461a2b6475b04ee099e49
S-Andres-Suarez <sebastian.suarez@ofi.com.co>
2023-01-04 11:27:35 -0500

    Ejercicio 3.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,54 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        //Punto 1
-       int numeroIf = 0;
-
-       if (numeroIf > 0){
-           System.out.println("NumeroIf es Positivo");
-       }if (numeroIf < 0){
-            System.out.println("NumeroIf es Negativo");
-        }if (numeroIf == 0){
-            System.out.println("NumeroIf es 0");
-        }
-
-       //Punto 2
-       int numeroWhile = 1;
-       while (numeroWhile < 3){
-           System.out.println("NumeroWhile es: "+numeroWhile);
-           numeroWhile = numeroWhile + 1;
-       }
-
-       //Punto 3
-       int numeroDoWhile = 2;
-       do {
-           System.out.println("NumeroDoWhile es: "+numeroDoWhile);
-           numeroDoWhile = numeroDoWhile + 3;
-       }while (numeroWhile < 3);
-
-       //Punto 4
-       for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
-           System.out.println("NumeroFor es: "+numeroFor);
-        }
-
-       //Punto 5
-       String Estacion = "Verano";
-       //Verano, Invierno, Otoño, Primavera
-       switch (Estacion){
-           case "Verano":
-               System.out.println("Verano");
-               break;
-           case "Invierno":
-               System.out.println("Invierno");
-               break;
-           case "Otoño":
-               System.out.println("Otoño");
-               break;
-           case "Primavera":
-               System.out.println("Primavera");
-               break;
-           default:
-               System.out.println(Estacion);
-       }
+        Persona persona1 = new Persona();
+        persona1.setEdad(20);
+        persona1.setNombre("Carlos");
+        persona1.setTelefono(3000);
+        System.out.println("La edad es: "+persona1.getEdad()+"\n"+"El numero de telefono es: "+
+                persona1.getTelefono()+"\n"+"El nombre es: "+persona1.getNombre());
     }
 }
---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Persona {
+        int edad;
+        String nombre;
+        double telefono;
+
+        public Persona() {
+
+        }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+}
